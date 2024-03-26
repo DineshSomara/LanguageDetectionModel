c9ab9dfec180b20dc19240b773d598b00dfd252c
dineshsomara04 <dineshsomara@gmail.com>
2024-03-27 03:29:48 +0530

    first commit

--- languageDetectionModel/backend/app.py
+++ languageDetectionModel/backend/app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from sklearn.model_selection import train_test_split
@@ -10,24 +9,24 @@ import pandas as pd
 app = Flask(__name__)
 CORS(app)
 
-# Load data from CSV file
+
 data = pd.read_csv('dataset.csv')
 texts = data['Text']
 labels = data['language']
 
-# Split the data into training and testing sets
+
 train_texts, test_texts, train_labels, test_labels = train_test_split(texts, labels, test_size=0.2, random_state=42)
 
-# Create a pipeline with CountVectorizer and MultinomialNB
+
 pipeline = Pipeline([
-    ('vectorizer', CountVectorizer()),  # Convert text to Bag-of-Words representation
-    ('classifier', MultinomialNB())     # Use Multinomial Naive Bayes classifier
+    ('vectorizer', CountVectorizer()),  
+    ('classifier', MultinomialNB())     
 ])
 
-# Fit the pipeline on the training data
+
 pipeline.fit(train_texts, train_labels)
 
-# Define endpoint for language prediction
+
 @app.route('/predict', methods=['POST'])
 def predict_language():
     data = request.json['text']
